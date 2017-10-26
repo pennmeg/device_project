b0326147e114fabbb583f6084f36eecff3cfdff3
Megan Penn <penn.meg@gmail.com>
2017-10-26 12:58:28 -0400

    started airplanes functions

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -3,5 +3,5 @@ class Airplane < ApplicationRecord
   belongs_to :airport
   belongs_to :photo
   has_many :job_planes
-  has_many :jobs, through :job_planes
+  has_many :jobs, through: :job_planes
 end
